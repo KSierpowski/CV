8e0e8514aca6186ae63c43832b7463622781c175
KSierpowski <sierpowskikacper@gmail.com>
2023-10-27 20:19:41 +0200

    Added projects area, with next/previous button

---
+++ Projects.js
@@ -0,0 +1,27 @@
+const myProjects = [
+    
+    {
+        id: 0,
+        image: "./images/project_1.png",
+        button: "View Project",
+        url: "https://github.com/KSierpowski/Webshop",
+    },
+    {
+        id: 1,
+        image: "./images/project_2.png",
+        button: "View Project",
+        url: "https://github.com/KSierpowski/RunRocket",
+    },
+    {
+        id: 2,
+        image: "./images/project_3.png",
+        button: "View Project",
+        url: "https://github.com/KSierpowski/todo-app",
+    },
+    {
+        id: 3,
+        image: "./images/project_4.png",
+        button: "View Project",
+        url: "https://github.com/KSierpowski/RunYouFool",
+    },
+]
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const whyMeSection = document.querySelector('.why_me');
 
 // Iterujemy przez tablicę reasons
 reasons.forEach(reason => {
-    // Tworzymy div, który będzie reprezentować jeden powód
+    // Tworzymy div, który będzie reprezentować reason
     const reasonDiv = document.createElement('div');
     reasonDiv.classList.add('reason');
   
@@ -26,4 +26,58 @@ reasons.forEach(reason => {
     });
   });
 
+
+  const projectsSection = document.querySelector('.my_projects');
+  const nextButton = document.querySelector('#nextButton');
+  const prevButton = document.querySelector('#prevButton');
+
+  let currentIndex = 0; 
+  let displayedProjectDiv = null; 
+  
+  // Funkcja, która usuwa poprzedni projekt i wyświetla nowy na podstawie aktualnego indeksu
+  function displayProject() {
+      if (displayedProjectDiv) {
+          projectsSection.removeChild(displayedProjectDiv); // Usuń poprzedni projekt
+      }
+  
+      const project = myProjects[currentIndex];
+      const projectDiv = document.createElement('div');
+      projectDiv.classList.add('project');
+  
+      const img = document.createElement('img');
+      img.src = project.image;
+  
+      const button = document.createElement('button');
+      button.textContent = project.button;
+  
+      projectDiv.appendChild(img);
+      projectDiv.appendChild(button);
+  
+      projectsSection.appendChild(projectDiv);
+      displayedProjectDiv = projectDiv; // Aktualizuj referencję do wyświetlanego projektu
+  
+      button.addEventListener('click', () => {
+          window.location.href = project.url;
+      });
+  }
+  
+  displayProject(); 
+  
+  nextButton.addEventListener('click', () => {
+      currentIndex++; 
+      if (currentIndex >= myProjects.length) {
+          currentIndex = 0; // Wróć do pierwszego projektu, jeśli przekroczono zakres
+      }
+      displayProject(); 
+  });
+  prevButton.addEventListener('click', () => {
+    currentIndex--;
+    if (currentIndex <0){
+        currentIndex=myProjects.length -1
+    } 
+    
+        
+    
+    displayProject(); 
+});
 
