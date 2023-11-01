00af5ea2d251f135f8e2f6330b632b52db89d7d7
KSierpowski <sierpowskikacper@gmail.com>
2023-11-01 11:57:09 +0100

    Added contact area

--- main.js
+++ main.js
@@ -181,3 +181,46 @@ inputElements.forEach(inputElement => {
         this.style.fontSize = '25px';
     });
 });
+
+emailjs.init("zvMWAfDPtt213CDnc");
+let allInformation = false;
+
+function redAlert(form) {
+    // Pobierz wszystkie pola formularza
+    const fields = form.querySelectorAll('input[name="subject"], input[name="fullName"], input[name="emailAdress"], textarea[name="message"]');
+
+    // Zresetuj kolory obramowania pól
+    fields.forEach(field => field.style.borderColor = "");
+
+    let allInformation = true;
+
+    // Sprawdź, czy pola są uzupełnione
+    fields.forEach(field => {
+        if (field.value === "") {
+            // Jeśli pole jest puste, zaznacz je na czerwono
+            field.style.borderColor = "red";
+            allInformation = false;
+        }
+    });
+
+    if (!allInformation) {
+        alert("Uzupełnij wymagane pola!");
+    }
+}
+document.querySelector(".contact_area").addEventListener("submit", function (e) {
+
+    e.preventDefault();
+    redAlert(this);
+if(allInformation){
+
+    emailjs.sendForm("service_jokvpnr","template_2kjetww", this)
+    .then(function (response) {
+        alert("Wiadomość została wysłana pomyślnie!");
+        document.querySelector(".contact_area").reset();
+    })
+    .catch(function (error) {
+        alert("Wystąpił błąd podczas wysyłania wiadomości: " + error);
+        document.querySelector(".contact_area").reset();
+    });
+}
+});
