2067ffa6c136cb67e3b4385ae301cd268b249d96
Kacper Sierpowski <sierpowskikacper@gmail.com>
2023-11-04 15:10:20 +0100

    Added carousel indicators

--- JavaScript/WhyMeCreativity.js
+++ JavaScript/WhyMeCreativity.js
@@ -1,7 +1,7 @@
 const photoGallery = document.querySelector('.photo_gallery');
 const prevButton = document.querySelector('.prev_button');
 const nextButton = document.querySelector('.next_button');
-
+const carousel_indicators = document.querySelectorAll('.carousel');
 let currentIndex = 0;
 let photos = photoGallery.querySelectorAll('img');
 
@@ -17,17 +17,19 @@ function displayPhoto() {
     }
 }
 
-displayPhoto();
 
 
 // Ustaw wysokość przycisku na wysokość aktualnego zdjęcia
-if (currentIndex >= 0 && currentIndex < photos.length) {
-    prevButton.style.height = photos[currentIndex].height + 'px';
-    nextButton.style.height = photos[currentIndex].height + 'px';
-}
 
+    if (currentIndex >= 0 && currentIndex < photos.length) {
+        prevButton.style.height = photos[currentIndex].height + 'px';
+        nextButton.style.height = photos[currentIndex].height + 'px';
+    }
+
+displayPhoto();
 nextButton.addEventListener('click', () => {
 
+    
     // Ukryj aktualne zdjęcie
     if (currentIndex >= 0 && currentIndex < photos.length) {
         photos[currentIndex].style.display = 'none';
@@ -37,14 +39,42 @@ nextButton.addEventListener('click', () => {
     if (currentIndex >= photos.length) {
         currentIndex = 0; // Wróć do pierwszego zdjęcia, jeśli przekroczono zakres
     }
-    
+
+    updateActiveDot();
     displayPhoto();
+
 });
 prevButton.addEventListener('click', () => {
     currentIndex--;
     if (currentIndex < 0){
         currentIndex=photos.length - 1;
     } 
-    
-    displayPhoto(); 
+    updateActiveDot();
+    displayPhoto();
   });
+
+
+  carousel_indicators.forEach((carousel, index) => {
+    carousel.addEventListener('click', () => {
+        currentIndex = index; // Aktualny indeks na podstawie klikniętego kółka
+        // Usuń klasę "active" z carousel, która była wcześniej aktywna
+        carousel_indicators.forEach(carousel => {
+            carousel.classList.remove('active');
+        });
+
+        // Dodaj klasę "active" do klikniętego kółka
+        carousel.classList.add('active');
+
+        displayPhoto();
+    });
+});
+
+function updateActiveDot() {
+    // Usuń klasę "active" ze wszystkich carousel
+    carousel_indicators.forEach(carousel => {
+        carousel.classList.remove('active');
+    });
+
+    // Dodaj klasę "active" do aktualnego carousel
+    carousel_indicators[currentIndex].classList.add('active');
+}
