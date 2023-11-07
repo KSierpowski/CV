b6469d836562b1b5765ebfb60ec782cfa8be9bb4
Kacper Sierpowski <sierpowskikacper@gmail.com>
2023-11-07 16:29:54 +0100

    Added tattoo gallery

---
+++ JavaScript/LearnersGallery.js
@@ -0,0 +1,34 @@
+
+function openGalerry() {
+  document.getElementById("my_gallery").style.display = "block";
+}
+
+// Close the Modal
+function closeGallery() {
+  document.getElementById("my_gallery").style.display = "none";
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("my_Slides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "flex";
+}
