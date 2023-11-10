3b3d4ab7d9048684f0fee3533c30c8adec471659
Kacper Sierpowski <sierpowskikacper@gmail.com>
2023-11-10 17:39:17 +0100

    Complementary information

--- JavaScript/WhyMe.js
+++ JavaScript/WhyMe.js
@@ -2,22 +2,19 @@ const reasons = [
     {
       id: 0,
       image: "./images/creative_icon.png",
-      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,
-      dolores eaque. Eligendi ab officia minus.`,
+      description: `A few reasons why I consider myself a creative person`,
       url: "./HTML/creativity.html",
     },
     {
       id: 1,
       image: "./images/experience_icon.png",
-      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,
-      dolores eaque. Eligendi ab officia minus.`,
+      description: `The most important work experience gathered in one place`,
       url: "./HTML/experience.html",
     },
     {
       id: 2,
       image: "./images/study_icon.png",
-      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,
-      dolores eaque. Eligendi ab officia minus.`,
+      description: `Private tattoo and sound production projects`,
       url: "./HTML/learners.html",
     },
   ];
