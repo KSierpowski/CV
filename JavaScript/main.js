0dd2a7ce468e483379d1e1768bf913b1eb1fffc6
Kacper Sierpowski <sierpowskikacper@gmail.com>
2023-11-11 12:38:33 +0100

    Added scroll to top button

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -7,8 +7,7 @@ const navLinks = document.querySelectorAll("header nav ul li a");
 let isMenuOpen = false;
 
 hamburgerIcon.addEventListener("click", () => {
-    isMenuOpen = !isMenuOpen; // Odwróć stan 
-
+    isMenuOpen = !isMenuOpen; 
     if (isMenuOpen) {
         navLinks.forEach(link => {
             link.style.display = "block"; // Pokaż linki
@@ -25,3 +24,22 @@ hamburgerIcon.addEventListener("click", () => {
         });
      }
 });
+
+window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    const scrollToTop = document.getElementById("scrollToTop");
+  
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTop.style.display = "block";
+    } else {
+      scrollToTop.style.display = "none";
+    }
+  }
+  
+  function scrollToTop() {
+    document.body.scrollTop = 0; // Dla wsparcia starszych przeglądarek
+    document.documentElement.scrollTop = 0; // Dla nowoczesnych przeglądarek
+  }
